a1ce06b1fa1a86698f4e717cb61fb754d98e1c11
marloncostaa <marloncostaa@gmail.com>
2017-06-21 18:43:16 -0300

    Update teste1.sh

--- teste1.sh
+++ teste1.sh
@@ -9,7 +9,7 @@ sudo apt-get install git make automake screen libcurl4-openssl-dev
 
 cd /opt
 
-git clone https://github.com/nicehash/cpuminer-multi
+git clone https://github.com/tpruvot/cpuminer-multi
 
 cd cpuminer-multi
 
