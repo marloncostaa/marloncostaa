9bc408c825bc197675c02bc6099e78a6e2f3978b
marloncostaa <marloncostaa@gmail.com>
2017-06-22 15:48:42 -0300

    Update and rename outrominergate.sh to 2 - minergate.sh

--- 2 - minergate.sh
+++ 2 - minergate.sh
@@ -1,17 +1,9 @@
 apt-get install autoconf autogen
-
 apt-get update
-
 sudo apt-get install git make automake gcc screen libcurl4-openssl-dev
-
 git clone https://github.com/wolf9466/cpuminer-multi
-
 cd cpuminer-multi
-
 ./autogen.sh
-
 CFLAGS="-march=native" ./configure --disable-aes-ni
-
 make
-
 ./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
