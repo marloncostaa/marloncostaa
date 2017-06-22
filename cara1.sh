42520d57ab3f559600417680de3870b71b3d5e94
marloncostaa <marloncostaa@gmail.com>
2017-06-21 23:09:05 -0300

    Create cara1.sh

---
+++ cara1.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get install libcurl4-openssl-dev git 
+apt-get install build-essential 
+apt-get install autotools-dev autoconf 
+apt-get install libcurl3 libcurl4-gnutls-dev
+mkdir /download
+cd /downloads
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi 
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+make install
+minerd -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.Linux -p x -t
