a147da8ca27af7387cd066cf0a2240f22b4be874
marloncostaa <marloncostaa@gmail.com>
2017-06-22 16:10:50 -0300

    Update testar1.sh

--- testar1.sh
+++ testar1.sh
@@ -13,5 +13,7 @@ cd cpuminer-multi
 CFLAGS="-march=native" ./configure --disable-aes-ni
 
 make
+./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
 
-./minerd -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg -p x -t 0
+#./minerd -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg -p x -t 0
+
