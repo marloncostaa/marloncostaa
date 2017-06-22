812a830dea4181af03d3e68ec074d04a515ad31b
marloncostaa <marloncostaa@gmail.com>
2017-06-22 01:04:31 -0300

    Create Minergatemelhor.sh

---
+++ Minergatemelhor.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
+ 
+minergate-cli -user bravulasco@gmail.com -bcn
+ 
+MINERAR SEM TERMINAL:
+nohup minergate-cli -user bravulasco@gmail.com -bcn 8
