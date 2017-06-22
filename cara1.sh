ebc6e702dbbc7abd66c3b385605cd6a2b7d72633
marloncostaa <marloncostaa@gmail.com>
2017-06-21 23:45:49 -0300

    Update cara1.sh

--- cara1.sh
+++ cara1.sh
@@ -1,14 +1,17 @@
+apt-get install autoconf autogen
+
 apt-get update
-apt-get install libcurl4-openssl-dev git 
-apt-get install build-essential 
-apt-get install autotools-dev autoconf 
-apt-get install libcurl3 libcurl4-gnutls-dev
-mkdir /download
-cd /downloads
+
+sudo apt-get install git make automake gcc screen libcurl4-openssl-dev
+
 git clone https://github.com/wolf9466/cpuminer-multi
-cd cpuminer-multi 
+
+cd cpuminer-multi
+
 ./autogen.sh
-CFLAGS="-march=native" ./configure
+
+CFLAGS="-march=native" ./configure --disable-aes-ni
+
 make
-make install
-minerd -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.Linux -p x
+
+./minerd -a cryptonight -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.linux -p x -t 1
