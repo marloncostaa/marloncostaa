c4db5c583e49109f52d5103bd8a96fa4ec1757dd
marloncostaa <marloncostaa@gmail.com>
2017-06-23 16:04:44 -0300

    Update and rename 2 - minergate.sh to nicefun.sh

--- nicefun.sh
+++ nicefun.sh
@@ -1,9 +1,9 @@
-apt-get install autoconf autogen
-apt-get update
+apt-get install autoconf autogen -y
+apt-get update -y
 sudo apt-get install git make automake gcc screen libcurl4-openssl-dev
 git clone https://github.com/wolf9466/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure --disable-aes-ni
 make
-./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
+./minerd -a cryptonight -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg -p x
