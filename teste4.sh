852d11df7cfbb14da9bda299de3ec75ee1412c00
marloncostaa <marloncostaa@gmail.com>
2017-06-24 16:05:12 -0300

    Update teste4.sh

--- teste4.sh
+++ teste4.sh
@@ -1,10 +1,5 @@
-sudo apt-get update -y
-sudo apt-get install cmake build-essential libboost-all-dev
-git clone -b Linux https://github.com/nicehash/nheqminer.git
-cd nheqminer/cpu_xenoncat/Linux/asm/
-sh assemble.sh
-cd ../../../Linux_cmake/nheqminer_cpu
-cmake .
-make -j $(nproc)
-./nheqminer_cpu -b
-./nheqminer_cpu -l equihash.hk.nicehash.com:3357 -u 1KxZfpBeizCxHxXsge4jQcKU5PK269CgUc.work1 -t 14
+apt-get update -y
+apt-get upgrade -y
+apt-get install lxde-core xfonts-base tightvncserver -y
+
+tightvncserver :1
