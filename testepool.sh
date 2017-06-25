7cfe7e705e1b9d1cc50261eccabea6f1b49495d8
marloncostaa <marloncostaa@gmail.com>
2017-06-24 21:31:16 -0300

    Update testepool.sh

--- testepool.sh
+++ testepool.sh
@@ -1,14 +1,11 @@
+apt-get install autoconf autogen -y
 apt-get update -y
-apt-get install libcurl4-openssl-dev git -y
-apt-get install build-essential -y
-apt-get install autotools-dev autoconf -y
-apt-get install libcurl3 libcurl4-gnutls-dev -y
-mkdir /download
-cd /downloads
+sudo apt-get install git make automake gcc screen libcurl4-openssl-dev -y
 git clone https://github.com/wolf9466/cpuminer-multi
-cd cpuminer-multi 
+cd cpuminer-multi
 ./autogen.sh
-CFLAGS="-march=native" ./configure
+CFLAGS="-march=native" ./configure --disable-aes-ni
 make
-make install
-minerd quark -o stratum+tcp://quark.mine.zpool.ca:4033 -u 78mT6C2kPTkSQo3ws4XwxbCQgZrMVmqKxN -p c=MUE
+./minerd -a quark -o stratum+tcp://quark.mine.zpool.ca:4033 -u 78mT6C2kPTkSQo3ws4XwxbCQgZrMVmqKxN -p c=MUE
+
+
