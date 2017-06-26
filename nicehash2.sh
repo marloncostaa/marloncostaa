7ccda18cd5087fafe5dccd1d471c7b5e43d538f4
marloncostaa <marloncostaa@gmail.com>
2017-06-26 14:32:05 -0300

    Delete nicehash2.sh

--- nicehash2.sh
+++
@@ -1,9 +0,0 @@
-apt-get install autoconf autogen -y
-apt-get update -y
-sudo apt-get install git make automake gcc screen libcurl4-openssl-dev -y
-git clone https://github.com/wolf9466/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-CFLAGS="-march=native" ./configure --disable-aes-ni
-make
-./minerd -a cryptonight -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.bravu2 -p x
