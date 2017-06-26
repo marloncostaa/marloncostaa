4e26b437ed25bdcc86890d232fd82338f5454283
marloncostaa <marloncostaa@gmail.com>
2017-06-26 14:40:50 -0300

    Create minergate2.sh

---
+++ minergate2.sh
@@ -0,0 +1,9 @@
+apt-get install autoconf autogen -y
+apt-get update -y
+sudo apt-get install git make automake gcc screen libcurl4-openssl-dev -y
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure --disable-aes-ni
+make
+./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
