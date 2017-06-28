e7939ebf704ce3867e2f2e5b89cb6992ae4ac171
marloncostaa <marloncostaa@gmail.com>
2017-06-28 14:07:01 -0300

    Update minergate2.sh

--- minergate2.sh
+++ minergate2.sh
@@ -6,4 +6,6 @@ cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure --disable-aes-ni
 make
-./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
+./minerd -a cryptonight -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg -p x
+
+# ./minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
