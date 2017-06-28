369639a99aa3417bb9a0cf0c958e31e424973eb7
marloncostaa <marloncostaa@gmail.com>
2017-06-27 22:23:17 -0300

    Update minergate1.sh

--- minergate1.sh
+++ minergate1.sh
@@ -13,7 +13,7 @@ make
 make install
 minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x -t 0
 
-# nohup minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x -t 0
+# nohup minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x -t 0 &
 # Ctrl-z
 # bg
 # nohup minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u bravulasco@gmail.com -p x
