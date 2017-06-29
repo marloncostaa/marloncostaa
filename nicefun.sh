2696013d86deeb5cde8deccacb6f0673b5438676
marloncostaa <marloncostaa@gmail.com>
2017-06-29 12:58:08 -0300

    Update nicefun.sh

--- nicefun.sh
+++ nicefun.sh
@@ -6,4 +6,4 @@ cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure --disable-aes-ni
 make
-./minerd -a cryptonight -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.linux -p x
+./minerd -o stratum+tcp://cryptonight.br.nicehash.com:3355 -u 3Djurb9hE3jxx3x4ZKePs6uZRodS71kqQg.linux -p x
