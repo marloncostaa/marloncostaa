fd0e7dabe8badb196c427160b5ab96a842b758e5
marloncostaa <marloncostaa@gmail.com>
2017-06-29 11:21:16 -0300

    Update minermelhor.sh

--- minermelhor.sh
+++ minermelhor.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
  
-minergate-cli -user bravulasco@gmail.com -bcn
+minergate-cli -user bravulasco@gmail.com -xmr
  
 # nohup minergate-cli -user bravulasco@gmail.com -bcn &
 
