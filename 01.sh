46ba4b9f79bfae53913803937cf6985953fcf991
marloncostaa <marloncostaa@gmail.com>
2017-06-30 17:20:03 -0300

    Update and rename minermelhor.sh to 01.sh

--- 01.sh
+++ 01.sh
@@ -2,7 +2,7 @@ sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-
  
 minergate-cli -user bravulasco@gmail.com -bcn
  
-# nohup minergate-cli -user bravulasco@gmail.com -bcn &
+nohup minergate-cli -user bravulasco@gmail.com -bcn &
 
 #Ctrl-z
 #bg
